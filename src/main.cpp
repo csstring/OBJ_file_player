c5ba3e542ced5ca7d65f079747e08ec91d75639a
schoe <schoe@student.42seoul.kr>
2023-06-07 20:08:07 +0900

    glm test

--- src/main.cpp
+++ src/main.cpp
@@ -2,9 +2,15 @@
 #include "glew.h"
 #include "glfw3.h"
 #include <iostream>
+#include <vector>
+#include <string>
 #include <stdexcept>
+#include <fstream>
+#include <algorithm>
+#include <sstream>
 #include "shader.hpp"
 #include "objectEnv.hpp"
+#include "glm/glm.hpp"
 GLFWwindow* window;
 GLuint vertexbuffer;
 const int WINDOW_WITH = 1024;
@@ -44,14 +50,44 @@ void vaoInit(){
     glBindBuffer(GL_ARRAY_BUFFER, vertexbuffer);
     glBufferData(GL_ARRAY_BUFFER, sizeof(g_vertex_buffer_data), g_vertex_buffer_data, GL_STATIC_DRAW);
 }
+std::vector<glm::vec3> loadOBJ(std::string path, std::vector<glm::vec3>& buffer, std::string& objName, std::string& metal){
+	std::ifstream objStream(path, std::ios::in);
+	std::stringstream ss;
 
-int main(){
+	std::vector<
+	ss << objStream.rdbuf();
+	std::string line;
+	while (std::getline(ss, line)){
+		std::stringstream tmpSS(line);
+		glm::vec3 
+		if (line[0] == 'o'){
+
+		} else if (line[0] == 'v'){
+
+		} else {
+			continue;
+		}
+	}
+}
+
+void bufferInit(std::string path){
+	glGenBuffers(1, &vertexbuffer);
+  glBindBuffer(GL_ARRAY_BUFFER, vertexbuffer);
+	std::vector<glm::vec3> v_vertex_buffer_data;
+	std::string objName;
+	std::string metal;
+	loadOBJ(path,v_vertex_buffer_data, objName, metal);
+	glBufferData(GL_ARRAY_BUFFER, sizeof(glm::vec3)*v_vertex_buffer_data.size(), &v_vertex_buffer_data[0], GL_STATIC_DRAW);
+}
+
+int main(int ac , char* av){
 
     try
     {
         glfwWindowInit();
         ft_glewInit();
         vaoInit();
+				bufferInit(av);
     }
     catch(const std::exception& e)
     {
@@ -74,120 +110,3 @@ int main(){
     } while( glfwGetKey(window, GLFW_KEY_ESCAPE ) != GLFW_PRESS && 
                 glfwWindowShouldClose(window) == 0 );
 }
-
-/*
-#include <stdio.h>
-#include <stdlib.h>
-
-// Include GLEW
-#include <glew.h>
-
-// Include GLFW
-#include <glfw3.h>
-GLFWwindow* window;
-#include <iostream>
-#include "shader.hpp"
-#include <unistd.h>
-int main( void )
-{
-	// Initialise GLFW
-	if( !glfwInit() )
-	{
-		fprintf( stderr, "Failed to initialize GLFW\n" );
-		getchar();
-		return -1;
-	}
-
-	glfwWindowHint(GLFW_SAMPLES, 4);
-	glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 3);
-	glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 3);
-	glfwWindowHint(GLFW_OPENGL_FORWARD_COMPAT, GL_TRUE); // To make MacOS happy; should not be needed
-	glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);
-
-	// Open a window and create its OpenGL context
-	window = glfwCreateWindow( 1024, 768, "Tutorial 02 - Red triangle", NULL, NULL);
-	if( window == NULL ){
-		fprintf( stderr, "Failed to open GLFW window. If you have an Intel GPU, they are not 3.3 compatible. Try the 2.1 version of the tutorials.\n" );
-		getchar();
-		glfwTerminate();
-		return -1;
-	}
-	glfwMakeContextCurrent(window);
-
-	// Initialize GLEW
-	glewExperimental = true; // Needed for core profile
-	if (glewInit() != GLEW_OK) {
-		fprintf(stderr, "Failed to initialize GLEW\n");
-		getchar();
-		glfwTerminate();
-		return -1;
-	}
-
-	// Ensure we can capture the escape key being pressed below
-	glfwSetInputMode(window, GLFW_STICKY_KEYS, GL_TRUE);
-
-	// Dark blue background
-	glClearColor(0.0f, 0.0f, 0.4f, 0.0f);
-
-	GLuint VertexArrayID;
-	glGenVertexArrays(1, &VertexArrayID);
-	glBindVertexArray(VertexArrayID);
-
-	// Create and compile our GLSL program from the shaders
-	GLuint programID = LoadShaders( "/Users/schoe/Desktop/scope/src/VertexShader", "/Users/schoe/Desktop/scope/src/FragmentShader" );
-
-
-	static const GLfloat g_vertex_buffer_data[] = { 
-		-1.0f, -1.0f, 0.0f,
-		 1.0f, -1.0f, 0.0f,
-		 0.0f,  1.0f, 0.0f,
-	};
-
-	GLuint vertexbuffer;
-	glGenBuffers(1, &vertexbuffer);
-	glBindBuffer(GL_ARRAY_BUFFER, vertexbuffer);
-	glBufferData(GL_ARRAY_BUFFER, sizeof(g_vertex_buffer_data), g_vertex_buffer_data, GL_STATIC_DRAW);
-
-	do{
-
-		// Clear the screen
-		glClear( GL_COLOR_BUFFER_BIT );
-
-		// Use our shader
-		glUseProgram(programID);
-
-		// 1rst attribute buffer : vertices
-		glEnableVertexAttribArray(0);
-		glBindBuffer(GL_ARRAY_BUFFER, vertexbuffer);
-		glVertexAttribPointer(
-			0,                  // attribute 0. No particular reason for 0, but must match the layout in the shader.
-			3,                  // size
-			GL_FLOAT,           // type
-			GL_FALSE,           // normalized?
-			0,                  // stride
-			(void*)0            // array buffer offset
-		);
-
-		// Draw the triangle !
-		glDrawArrays(GL_TRIANGLES, 0, 3); // 3 indices starting at 0 -> 1 triangle
-
-		glDisableVertexAttribArray(0);
-
-		// Swap buffers
-		glfwSwapBuffers(window);
-		glfwPollEvents();
-
-	} // Check if the ESC key was pressed or the window was closed
-	while( glfwGetKey(window, GLFW_KEY_ESCAPE ) != GLFW_PRESS &&
-		   glfwWindowShouldClose(window) == 0 );
-
-	// Cleanup VBO
-	glDeleteBuffers(1, &vertexbuffer);
-	glDeleteVertexArrays(1, &VertexArrayID);
-	glDeleteProgram(programID);
-
-	// Close OpenGL window and terminate GLFW
-	glfwTerminate();
-
-	return 0;
-}*/
